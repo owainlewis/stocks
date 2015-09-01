33c2e3237d0a2922d51323c32082b8fe5f64936d
Owain Lewis <owain@owainlewis.com>
2015-09-01 09:27:04 +0100

    Update utils.clj

--- src/stocks/utils.clj
+++ src/stocks/utils.clj
@@ -7,9 +7,6 @@
   (with-open [rdr (io/reader path)]
     (into [] (line-seq rdr))))
 
-(defn split-comma [s]
-  (clojure.string/split s #","))
-
 (defn load-csv [path]
   (when-let [lines (read-lines path)]
     (mapv #(clojure.string/split % #",") lines)))
